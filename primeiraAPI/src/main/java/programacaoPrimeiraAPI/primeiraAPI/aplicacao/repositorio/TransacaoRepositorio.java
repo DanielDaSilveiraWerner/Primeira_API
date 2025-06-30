274f6422b668e7556017a7a8be048521bbe58bce
Daniel da Silveira Werner <wernerd107@gmail.com>
2025-06-29 21:12:54 -0300

    termindo o repositorio

--- primeiraAPI/src/main/java/programacaoPrimeiraAPI/primeiraAPI/aplicacao/repositorio/TransacaoRepositorio.java
+++ primeiraAPI/src/main/java/programacaoPrimeiraAPI/primeiraAPI/aplicacao/repositorio/TransacaoRepositorio.java
@@ -1,7 +1,10 @@
 package programacaoPrimeiraAPI.primeiraAPI.aplicacao.repositorio;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
@@ -26,5 +29,40 @@ public void deletaTransacao(){
 public List<Transacao> listaTransacaos(){
     return new ArrayList<>(listar);
 }
+public List<Transacao> encontraHoraDepois (LocalDateTime limit){
+List<Transacao> recente = new ArrayList<>();
+for (Transacao transacao : listar){
+    if (transacao.getDataHora()!= null && 
+    (transacao.getDataHora().isEqual(limit) || transacao.getDataHora().isAfter(limit))) {
+        recente.add(transacao);
+    }
+}
+return recente;
+}
 
+ public List<Transacao> findByDataHoraBetween(LocalDateTime dataInicial, LocalDateTime dataFinal) {
+        return listar.stream()
+            .filter(i -> {
+                if (i.getDataHora() == null) return false;
+                return (i.getDataHora().isEqual(dataInicial) || i.getDataHora().isAfter(dataInicial))
+                    && (i.getDataHora().isEqual(dataFinal) || i.getDataHora().isBefore(dataFinal));
+            })
+            .collect(Collectors.toList());
+    }
+
+
+    public Optional<Transacao> findLastTransacao() {
+        return listar.stream()
+            .filter(i -> i.getDataHora() != null)
+            .max((i1, i2) -> i1.getDataHora().compareTo(i2.getDataHora()));
+    }
+ 
+    public void limparDataHora (LocalDateTime dataInicial, LocalDateTime dataFinal){
+        listar.removeIf(i ->{
+            if (i.getDataHora()== null) return false;
+            return (i.getDataHora().isEqual(dataInicial) || i.getDataHora().isAfter(dataInicial))
+            && (i.getDataHora().isEqual(dataFinal) || i.getDataHora().isBefore(dataFinal)); 
+                
+        });
+    }
 }
