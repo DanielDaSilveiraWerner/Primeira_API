bae828640cbfb646c3ce9311f5ef72d290ac1657
Daniel da Silveira Werner <wernerd107@gmail.com>
2025-07-02 11:04:09 -0300

    implementado banco3

--- primeiraAPI/src/main/java/programacaoPrimeiraAPI/primeiraAPI/aplicacao/servicos/bancos/banco3.java
+++ primeiraAPI/src/main/java/programacaoPrimeiraAPI/primeiraAPI/aplicacao/servicos/bancos/banco3.java
@@ -1,5 +1,44 @@
 package programacaoPrimeiraAPI.primeiraAPI.aplicacao.servicos.bancos;
 
-public class banco3 {
+import java.time.LocalDateTime;
+import java.util.List;
+
+import programacaoPrimeiraAPI.primeiraAPI.aplicacao.dto.TransacaoDTO;
+import programacaoPrimeiraAPI.primeiraAPI.aplicacao.model.Transacao;
+import programacaoPrimeiraAPI.primeiraAPI.aplicacao.repositorio.TransacaoRepositorio;
+import programacaoPrimeiraAPI.primeiraAPI.aplicacao.servicos.interfaces.TransacaoServico;
+
+public class banco3 implements TransacaoServico{
+
+
+    private final TransacaoRepositorio repositorio;
+
+    public banco3 (TransacaoRepositorio repositorio){
+        this.repositorio = repositorio;
+    }
+    @Override
+    public Transacao salvarTransacao(TransacaoDTO dto) {
+        Transacao transacao = new Transacao(dto);
+        repositorio.adicionaTransacao(transacao);
+        return transacao;
+    }
+
+    @Override
+    public List<Transacao> listarTransacoes() {
+        return repositorio.listaTransacaos();
+    }
+
+    @Override
+    public void deletarTodasTransacoes() {
+         repositorio.deletaTransacao();
+    }
+
+    @Override
+    public void deletarPorPeriodo(LocalDateTime inicio, LocalDateTime fim, String senha) {
+        if (!"BD3@789".equals(senha)) {
+            throw new RuntimeException("Senha inválida");
+        }
+        repositorio.limparDataHora(inicio, fim);
+    }
     
 }
